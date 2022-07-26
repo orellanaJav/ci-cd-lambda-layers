e1144a67c74d7bcf2eaa19b0fc24c03a3fbb1bed
Javier Orellana <javier.orellana@destacame.cl>
2022-07-26 19:03:30 -0400

    Updated new layer

--- test_layer_bifrost/test_layer_bifrost/test_layer_bifrost.py
+++ test_layer_bifrost/test_layer_bifrost/test_layer_bifrost.py
@@ -1,10 +1,10 @@
-from typing import dict
+from typing import Dict
 
 
 __version__ = '0.1.0'
 
 
-def test_layer_bifrost(params: dict) -> bool:
+def test_layer_bifrost(params: Dict) -> bool:
     """
     test_layer
 
