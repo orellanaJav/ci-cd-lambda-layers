7412641cca05dbd1759af353540ed8ac91cd95a1
Javier Orellana <javier.orellana@destacame.cl>
2022-07-26 19:56:28 -0400

    Added Test

--- unit_test_simple/unit_test_simple/unit_test_simple.py
+++ unit_test_simple/unit_test_simple/unit_test_simple.py
@@ -3,10 +3,10 @@ __version__ = '0.1.0'
 
 def unit_test_simple(params: dict) -> bool:
     """
-
+    Prueba de test unitario
 
     :Authors:
-        -
+        - Javier Orellana
 
     :Created:
         - 2022.07.26
