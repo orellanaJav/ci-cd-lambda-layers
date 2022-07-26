cd1b6d1ddff9cc9491b20e85fb9ff11397b87f10
Javier Orellana <javier.orellana@destacame.cl>
2022-07-26 18:49:27 -0400

    Added new layer

---
+++ test_layer_from_bifrost/test_layer_from_bifrost/test_layer_from_bifrost.py
@@ -0,0 +1,17 @@
+from typing import dict
+
+
+__version__ = '0.1.0'
+
+
+def test_layer_from_bifrost(params: dict) -> bool:
+    """
+    test from bifrost
+
+    :Authors:
+        -
+
+    :Created:
+        - 2022.07.26
+    """
+    return True
